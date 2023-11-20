4bd87c22eafc739e82fc6ad49093ef1025769ccb
felipe fontes <felipefonteslisboa@gmail.com>
2023-11-20 12:14:53 -0300

    Errow

--- acesso.php
+++ acesso.php
@@ -4,9 +4,16 @@
 include("conect.php");
 
  
+//requisiçao para logar
 //recebe cpf e senha do formulario de login index
 $cpf = $_REQUEST["cpf"];
 $senha = $_REQUEST ["senha"];
-//requisiçao para logar
+$sql = "SELECT * FROM usuario_admin WHERE cpf = '$cpf' AND senha = '$senha' " ;
 
+$resultado = mysqli_query($conect, $sql);
+// echo $cpf;
+// echo $senha;
+
+//PRINTS VERIFICATION REGISTER
+echo mysqli_num_rows($resultado);
 ?>
